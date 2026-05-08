8561a79aac5eee0751bfd8cd9b46207699389297
Kamil <kmirosz6@gmail.com>
2026-05-08 09:01:58 +0200

    Add serializers, views, and URLs for CinemaHall and Genre models
    - Updated .gitignore to exclude db.sqlite3
    - Implemented serializers for CinemaHall and Genre
    - Created viewsets for CinemaHall and Genre
    - Configured URLs to include cinema app routes

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,1 +1,18 @@
-# write serializers here
+from rest_framework import serializers
+
+
+from cinema.models import CinemaHall, Genre
+
+
+class CinemaHallSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = CinemaHall
+        fields = "__all__"
+        extra_kwargs = {"url": {"view_name": "cinema:cinemahall-detail"}}
+
+
+class GenreSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Genre
+        fields = "__all__"
+        extra_kwargs = {"url": {"view_name": "cinema:genre-detail"}}
--- cinema/urls.py
+++ cinema/urls.py
@@ -1,1 +1,16 @@
-# write urls here
+from django.urls import include, path
+from rest_framework import routers
+
+
+from cinema.views import CinemaHallViewSet, GenreHallViewSet
+
+
+router = routers.DefaultRouter()
+router.register(r"cinema-halls", CinemaHallViewSet, basename="cinemahall")
+router.register(r"genres", GenreHallViewSet, basename="genre")
+
+
+urlpatterns = [path("", include(router.urls))]
+
+
+app_name = "cinema"
